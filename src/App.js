72abb41a39529a7915e3adce27921aab7282aa7a
guilhermestain <guilhermestain@gmail.com>
2020-01-28 14:01:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    start: false,
+    message: "- Press the button start -"
+  };
+
+  changeMessage = () => {
+    this.setState({
+      message: "dwadwa"
+    });
+  };
+
+  start = () => {
+    this.setState({
+      start: true
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div class="main-game">
+          <div class="div-header">
+            <div class="div-menor-left-header"></div>
+            <div class="div-maior-header"></div>
+            <div class="div-menor-right-header"></div>
+          </div>
+          <div class="screen-game">
+            <div class="screen-header">
+              <div class="lines-header">
+                <div class="red-line"></div>
+                <div class="blue-line"></div>
+              </div>
+            </div>
+            <div class="div-screen-menor">
+              <div class="battery-screen">
+                <div class="ball-screen">
+                  <div class="ball-green"></div>
+                </div>
+                <div class="battery-game">BATTERY</div>
+              </div>
+              <div class="screen-menor-game">
+                <p class="a">
+                  {!this.state.start ? (
+                    // eslint-disable-next-line jsx-a11y/alt-text
+                    <img className="img" src="mario-start.png" />
+                  ) : (
+                    // eslint-disable-next-line jsx-a11y/alt-text
+                    <img className="img" src="gameboy-mario.png" />
+                  )}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="div-text">
+            <div class="nintendo-text">Nintendo</div>
+            <div class="gameBoy-text">GAME BOY ™</div>
+          </div>
+          <div class="div-buttons">
+            <div class="joistick">
+              <div class="reto"></div>
+              <div class="deitado"></div>
+            </div>
+            <div class="div-buttonsAB">
+              <div class="buttonsAB">
+                <div class="ball-A" onClick={this.changeMessage}></div>
+                <div class="ball-A"></div>
+              </div>
+              <div class="text-AB">
+                <div class="A-text">A</div>
+                <div class="B-text">B</div>
+              </div>
+            </div>
+          </div>
+          <div class="div-buttons-start">
+            <div class="div-start">
+              <div class="start"></div>
+              <div class="select-text">SELECT</div>
+            </div>
+            <div class="div-start">
+              <div class="start" onClick={this.start}></div>
+              <div class="start-text">START</div>
+            </div>
+          </div>
+          <div class="div-volume">
+            <div class="volume">
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+              <div class="mini-volumes"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
